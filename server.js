d7b809950662ca1a296ba155eddfa89161427b0d
JacyAnderson <jacyjamesanderson@gmail.com>
2017-04-19 10:48:07 -0600

    add ability to access a single park via command line in get request

--- server.js
+++ server.js
@@ -15,20 +15,25 @@ var flash        = require('connect-flash');
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var session      = require('express-session');
+var exec = require('child_process').exec;
+var cmd = 'curl -X GET \
+  https://developer.nps.gov/api/v0/parks?parkCode=romo\
+  -H "authorization: C7D61596-3FA5-47EA-906D-202B2B3ECA00" \
+  -H "cache-control: no-cache"';
 var authorization = "Authorization";
 var api_key = process.env.NPS_KEY;
-var url = 'https://developer.nps.gov/api/v0/parks?parkCode=romo';
+var url = 'https://developer.nps.gov/api/v0/parks';
 // var url = 'https://developer.nps.gov/api/v0/alerts?parkCode=yell,yose';
 
  
-var options = {
-  url: url,
-  headers: {
-    'Authorization': api_key
-  }
-};
+// var options = {
+//   url: url,
+//   headers: {
+//     'Authorization': api_key
+//   }
+// };
 
-console.log(options);
+// console.log(options);
 
 app.use(morgan('dev')); 
 app.use(cookieParser());
@@ -65,28 +70,37 @@ var port = process.env.PORT || 3000;
 
 var router = express.Router();
 
+
+
+// GET all National Parks
 app.get('/parks', function(req, res) {
 
 	console.log("Making get request at Index");
     console.log("Your api_key is: " + api_key);	
-
-	request(options, function(err, response, body) {
-    console.log("In request");
-    if (!err && response.statusCode == 200) {
-      console.log('Here comes dat boi!!!');
-      var info = JSON.parse(body);
-      res.json(info);
-    } else if (!err && response.statusCode != 200) {
-      console.log("There is an error");
-      console.log(body);
-      console.log("The error is a " + response.statusCode);
-      res.send(body);
-    } else if(err) {
-      console.log("Error incoming");
-      console.log(body);
-      res.send("FAILURE");
-    } 
-  });
+    
+    exec(cmd, function(err, stdout, stderr) {
+      console.log(err);
+      console.log(stdout);
+      console.log(stderr);
+      res.send(stdout);
+    });
+	// request(options, function(err, response, body) {
+ //    console.log("In request");
+ //    if (!err && response.statusCode == 200) {
+ //      console.log('Here comes dat boi!!!');
+ //      var info = JSON.parse(body);
+ //      res.json(info);
+ //    } else if (!err && response.statusCode != 200) {
+ //      console.log("There is an error");
+ //      console.log(body);
+ //      console.log("The error is a " + response.statusCode);
+ //      res.send(body);
+ //    } else if(err) {
+ //      console.log("Error incoming");
+ //      console.log(body);
+ //      res.send("FAILURE");
+ //    } 
+  // });
 });
 
 
