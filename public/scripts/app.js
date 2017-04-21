06c5cb9db80679a8a17f11b4c2155b1231ce4c88
JacyAnderson <jacyjamesanderson@gmail.com>
2017-04-20 20:38:32 -0600

    add select function to console log selected park object

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,14 +3,28 @@
 $(document).ready(function() {
   console.log("Hello world!");
   getParkNames();
+  $('.select-box').change(function() {
+  	// var selected = $('.select-box option:selected').val();
+    // console.log($('.select-box option:selected').val());
+    searchParks();
+  	});
 });
 
 
 function getParkNames() {
 	$.get('/api/parks', function(parks) { 
 		parks.forEach(function(park){
-			$('.select-box').append("<option>" + park.fullName + "</option>").attr('park-id', park.id);  
+		  $('.select-box').append("<option>" + park.fullName + "</option>");
+		});
+	});
+};
+
+function searchParks() {
+	$.get('/api/parks', function(parks) {
+		parks.forEach(function(park) {
+			if ($('.select-box option:selected').val() === park.fullName) {
+              console.log(park);
+			}
 		});
 	});
 }
-
